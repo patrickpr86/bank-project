b75e13b07bfdcc5a03e3e0b8ea21a9315355f5b8
patrickpr86 <sh4dbr@gmail.com>
2020-10-06 15:49:42 -0300

    Project created

---
+++ backend/src/main/java/com/patrick/bankdemo/BankdemoApplication.java
@@ -0,0 +1,13 @@
+package com.patrick.bankdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankdemoApplication.class, args);
+	}
+
+}
